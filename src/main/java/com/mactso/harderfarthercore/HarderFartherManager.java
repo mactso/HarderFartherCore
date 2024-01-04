4037e799a947bba5e76d13316e9545660cea0920
mactso <stephenmcleod2004@yahoo.com>
2024-01-03 20:25:48 -0600

    HarderFartherCore Version 0.1 for Minecraft 1.20
    Prototype

---
+++ src/main/java/com/mactso/harderfarthercore/HarderFartherManager.java
@@ -0,0 +1,11 @@
+package com.mactso.harderfarthercore;
+
+import net.minecraft.core.BlockPos;
+
+public class HarderFartherManager   {
+	
+	public static float getDifficulty (BlockPos pos) { 
+	// return HarderFartherManager.getDifficulty (pos);
+		return 0.67f;
+	}
+}
---
+++ src/main/java/com/mactso/harderfarthercore/Main.java
@@ -0,0 +1,15 @@
+package com.mactso.harderfarthercore;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("harderfarthercore")
+public class Main {
+
+	    public static final String MODID = "harderfarthercore"; 
+	    
+	    public Main()
+	    {
+	    	System.out.println(MODID + ": Registering Mod.");
+	    }
+
+}
